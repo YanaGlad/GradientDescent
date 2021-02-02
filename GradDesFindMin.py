48fc734c4034810c5eed1b33e186c1338ee01235
YanaGlad <olesya_leader@mail.ru>
2021-02-02 23:44:27 +0300

    finished GradDesFindMin

--- GradDesFindMin.py
+++ GradDesFindMin.py
@@ -97,3 +97,48 @@ def test_convergence_1d(grad_descent, test_cases, tol=1e-2, axes=None, grid=None
         return right_flag, debug_log
 
 
+test_cases = {
+    "square": {
+        "func" : lambda x: x * x,
+        "deriv" : lambda x: 2 * x,
+        "start" : 2,
+        "answer" : 0.0
+    },
+    "module": {
+        "func" : lambda x: abs(x),
+        "deriv" : lambda x: 1 if x > 0 else -1,
+        "start" : 2,
+        "answer" : 0.0
+    },
+    "third_power": {
+        "func" : lambda x: abs((x - 1)**3),
+        "deriv" : lambda x: 3 * (x - 1)**2 * np.sign(x-1),
+        "start" : -1,
+        "answer" : 1.0
+    },
+    "ln_x2_1": {
+        "func" : lambda x: np.log((x + 1)**2 + 1),
+        "deriv" : lambda x: 2 * (x + 1) / (x**2 +1),
+        "start" : 1,
+        "answer" : -1.0
+    }
+}
+
+
+
+tol = 1e-2
+fig, axes = plt.subplots(2,2, figsize=(14, 12))
+fig.suptitle("Grad descent ver 1", fontweight="bold", fontsize=20)
+grid = np.linspace(-2, 2, 100)
+
+is_correct, debug_log = test_convergence_1d(
+    grad_descent_v1, test_cases, tol,
+    axes, grid
+)
+plt.show()
+
+if not is_correct:
+    print("Incorrect")
+    for log_entry in debug_log:
+        print(log_entry)
+
