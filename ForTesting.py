9f5b5c24f078b8531f0ed14a0359a50e9b2ebd40
YanaGlad <olesya_leader@mail.ru>
2021-02-03 15:24:58 +0300

    GradDesFindMutiMin finished

---
+++ ForTesting.py
@@ -0,0 +1,80 @@
+from copy import copy, deepcopy
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def plot_convergence_1d(func, x_steps, y_steps, ax, grid=None, title=""):
+    ax.set_title(title, fontsize=16, fontweight='bold')
+
+    if grid is None:
+        grid = np.linspace(np.min(x_steps), np.max(x_steps), 100)
+
+    fgrid = [func(item) for item in grid]
+    ax.plot(grid, fgrid)
+    yrange = np.max(fgrid) - np.min(fgrid)
+
+    arrow_kwargs = dict(linestyle="--", color="grey", alpha=0.4)
+
+    for i, _ in enumerate(x_steps):
+        if i + 1 < len(x_steps):
+            ax.arrow(
+                x_steps[i], y_steps[i],
+                x_steps[i + 1] - x_steps[i],
+                y_steps[i + 1] - y_steps[i],
+                **arrow_kwargs
+            )
+
+    n = len(x_steps)
+    color_list = [(i / n, 0, 0, 1 - i / n) for i in range(n)]
+    ax.scatter(x_steps, y_steps, c=color_list)
+    ax.scatter(x_steps[-1], y_steps[-1], c="red")
+    ax.set_xlabel(r"$x$")
+    ax.set_ylabel(r"$y$")
+
+
+class LoggingCallback:
+    def __init__(self):
+        self.x_steps = []
+        self.y_steps = []
+
+    def __call__(self, x, y):
+        self.x_steps.append(x)
+        self.y_steps.append(y)
+
+
+def test_convergence_1d(grad_descent, test_cases, tol=1e-2, axes=None, grid=None):
+    right_flag = True
+    debug_log = []
+
+    for i, key in enumerate(test_cases.keys()):
+        answer = test_cases[key]["answer"]
+        test_input = deepcopy(test_cases[key])
+        del test_input["answer"]
+
+        callback = LoggingCallback()
+        res_point = grad_descent(**test_input, callback=callback)
+
+        if axes is not None:
+            ax = axes[np.unravel_index(i, shape=axes.shape)]
+            x_steps = np.array(callback.x_steps)
+            y_steps = np.array(callback.y_steps)
+            plot_convergence_1d(
+                test_input["func"], x_steps, y_steps, ax, grid, key
+            )
+            ax.axvline(answer, 0, linestyle="--", c="red",
+                       label=f"true answer = {answer}")
+
+            ax.axvline(x_steps[-1], 0, linestyle="--", c="xkcd:tangerine",
+                       label=f"estimate = {np.round(x_steps[-1], 3)}")
+            ax.legend(fontsize=16)
+
+            if abs(answer - res_point) > tol or np.isnan(res_point):
+                debug_log.append(
+                    f"Test  'key' :\n"
+                    f"\t - answer: {answer}\n"
+                    f"\t - counted: {res_point}"
+                )
+                right_flag = False
+
+        return right_flag, debug_log
+
--- GradDesFinMultiMin.py
+++ GradDesFinMultiMin.py
@@ -2,8 +2,10 @@ from copy import copy, deepcopy
 import numpy as np
 import matplotlib.pyplot as plt
 
-
 # Finds global local min out of local mins
+from ForTesting import test_convergence_1d
+
+
 def grad_descent_v2(func, deriv, low=None, high=None, callback=None):
     if low is None:
         low = -4
@@ -20,9 +22,59 @@ def grad_descent_v2(func, deriv, low=None, high=None, callback=None):
             estimate_last = estimate
             estimate -= eps * deriv(estimate)
             callback(estimate, func(estimate))
-            if abs(estimate_last - estimate) < eps**2:
+            if abs(estimate_last - estimate) < eps ** 2:
                 break
             if func(best_estimate) > func(estimate):
                 best_estimate = estimate
 
     return best_estimate
+
+
+test_cases = {
+    "poly1": {
+        "func": lambda x: x ** 4 + 3 * x ** 3 + x ** 2 - 1.5 * x + 1,
+        "deriv": lambda x: 4 * x ** 3 + 9 * x ** 2 + 2 * x - 1.5,
+        "low": -3, "high": 3, "answer": -1.88
+    },
+    "poly2": {
+        "func": lambda x: x ** 4 + 3 * x ** 3 + x ** 2 - 2 * x + 1.0,
+        "deriv": lambda x: 4 * x ** 3 + 9 * x ** 2 + 2 * x - 2.0,
+        "low": -3, "high": 3, "answer": 0.352
+    },
+    "another poly": {
+        "func": lambda x: x ** 6 + x ** 4 - 10 * x ** 2,
+        "deriv": lambda x: 6 * x ** 5 + 4 * x ** 3 - 20 * x,
+        "low": 0, "high": 2, "answer": 1 / 3 * np.sqrt((np.sqrt(31) - 1) * 3)
+    },
+    "another yet poly": {
+        "func": lambda x: x ** 6 + x ** 4 - 10 * x ** 2 - x,
+        "deriv": lambda x: 6 * x ** 5 + 4 * x ** 3 - 20 * x - 1,
+        "low": -2, "high": 2, "answer": 1.24829
+    },
+    "and another yet poly": {
+        "func": lambda x: x ** 20 + x ** 2 - 20 * x + 10,
+        "deriv": lambda x: 20 * x ** 19 + 2 * x - 20,
+        "low": -1, "high": 2, "answer": 0.994502
+    },
+    "|x|/x^2 - x + sqrt(-x) + (even polynom)": {
+        "func": lambda x: 5 * np.abs(x) / x ** 2 - 0.5 * x + 0.1 * np.sqrt(-x) + 0.01 * x ** 2,
+        "deriv": lambda x: -0.5 - 0.05 / np.sqrt(-x) + 0.02 * x + 5 / (x * np.abs(x)) - (10 * np.abs(x)) / x ** 3,
+        "low": -5, "high": -2, "answer": -2.91701
+    },
+}
+
+tol = 1e-2  # желаемая точность
+
+fig, axes = plt.subplots(2, 4, figsize=(24, 8))
+fig.suptitle("Grad Descent ver 2", fontweight="bold", fontsize=20)
+grid = np.linspace(-3, 3, 100)
+
+is_correct, debug_log = test_convergence_1d(
+    grad_descent_v2, test_cases, tol,
+    axes, grid
+)
+
+if not is_correct:
+    print("Incorrect")
+    for log_entry in debug_log:
+        print(log_entry)
