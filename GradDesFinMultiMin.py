ed3c486fdc54e243ccfaa72a9f053f56971673b8
YanaGlad <olesya_leader@mail.ru>
2021-02-03 15:06:55 +0300

    finished func

--- GradDesFinMultiMin.py
+++ GradDesFinMultiMin.py
@@ -2,9 +2,9 @@ from copy import copy, deepcopy
 import numpy as np
 import matplotlib.pyplot as plt
 
+
 # Finds global local min out of local mins
 def grad_descent_v2(func, deriv, low=None, high=None, callback=None):
-
     if low is None:
         low = -4
     if high is None:
@@ -16,6 +16,13 @@ def grad_descent_v2(func, deriv, low=None, high=None, callback=None):
     best_estimate = low
 
     for estimate in range(low, high):
-        
+        for i in range(N):
+            estimate_last = estimate
+            estimate -= eps * deriv(estimate)
+            callback(estimate, func(estimate))
+            if abs(estimate_last - estimate) < eps**2:
+                break
+            if func(best_estimate) > func(estimate):
+                best_estimate = estimate
 
     return best_estimate
